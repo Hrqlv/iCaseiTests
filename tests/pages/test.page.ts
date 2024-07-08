d883d26ef0669b990417093bca0d84de4f4e61c3
Hrqlv <henriquelopesvelozo@gmail.com>
2024-07-08 12:21:37 -0300

    Feito um script para tests em geral e validaçoes da pagina inicial page e spec

--- tests/pages/test.page.ts
+++ tests/pages/test.page.ts
@@ -8,7 +8,14 @@ export default class PaginaInicialPage {
     }
 
     async realizarValidacoesPaginaInicial() {
-
+        await expect(this.page.locator('h1[class="sc-8e489586-4 gUmMra"]').filter({ hasText: 'Seu site de casamento merece ser iCasei' })).toBeVisible()
+        await expect(this.page.locator('h2[class="sc-b37cf185-6 fBIfKY"]').filter({ hasText: 'Prático, interativo e inesquecível!' })).toBeVisible()
+        await expect(this.page.locator('h2[class="sc-7298c4a9-4 cmASwm"]').filter({ hasText: 'Modelos de layouts premium, feitos por quem entende' })).toBeVisible()
+        await expect(this.page.locator('h2[class="sc-ecdb0b98-5 elKhlr"]').filter({ hasText: 'Lista de casamento em dinheiro' })).toBeVisible()
+        await expect(this.page.locator('h2[class="sc-600d3a62-5 hlEbw"]').filter({ hasText: 'Fique por dentro de todos os detalhes!' })).toBeVisible()
+        await expect(this.page.locator('h2[class="sc-de68d777-1 eEBsjM"]').filter({ hasText: 'Além de mais de 150 recursos incríveis!' })).toBeVisible()
+        await expect(this.page.locator('h2[class="sc-dc5ae993-1 kPnJaP"]').filter({ hasText: 'iCasei na mídia' })).toBeVisible()
+        await expect(this.page.locator('h2[class="sc-e540dd51-1 qomPe"]').filter({ hasText: 'Quem usou, aprovou e recomenda!' })).toBeVisible()   
     }
 
 }
--- tests/specs/example.spec.ts
+++ tests/specs/example.spec.ts
@@ -3,21 +3,21 @@ import PaginaInicialPage from '../pages/test.page';
 
 let paginaInicial: any
 
-test.describe('', () => {
+test.describe('iCasei - Automation Tests', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://teste-qa.icasei.com.br/')
     paginaInicial = new PaginaInicialPage(page);
   })
 
-  test('', async ({ page }) => {
-    await test.step('', async () => {
-
+  test('Realizar validaçoes pagina inicial', async ({ page }) => {
+    await test.step('Vaidaçoes pagina inicial', async () => {
+      await paginaInicial.realizarValidacoesPaginaInicial()
     })
   })
 
-  test('', async ({ page }) => {
-    await test.step('', async () => {
+  // test('', async ({ page }) => {
+  //   await test.step('', async () => {
 
-    })
-  })
+  //   })
+  // })
 })
